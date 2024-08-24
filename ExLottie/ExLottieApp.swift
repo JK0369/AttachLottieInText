d19675cab571cccc1fab59e2b111d4a7ef4d096a
jong <palatable7@naver.com>
2024-08-24 23:03:31 +0900

    Initial Commit

---
+++ ExLottie/ExLottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExLottieApp.swift
+//  ExLottie
+//
+//  Created by 김종권 on 2024/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExLottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
